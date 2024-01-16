dd1ff4968013e3825c4f80cc6855f9b0af94740a
henintsoa.andrianarisoa <henintsoa.andrianarisoa@businessdecision.com>
2024-01-17 00:22:33 +0400

    Refactorisation en fonction

--- delete-article.php
+++ delete-article.php
@@ -1,11 +1,6 @@
 <?php
-
-/**
- * DANS CE FICHIER, ON CHERCHE A SUPPRIMER L'ARTICLE DONT L'ID EST PASSE EN GET
- * 
- * Il va donc falloir bien s'assurer qu'un paramètre "id" est bien passé en GET, puis que cet article existe bel et bien
- * Ensuite, on va pouvoir effectivement supprimer l'article et rediriger vers la page d'accueil
- */
+require_once 'librairies/database.php';
+require_once 'librairies/utils.php';
 
 /**
  * 1. On vérifie que le GET possède bien un paramètre "id" (delete.php?id=202) et que c'est bien un nombre
@@ -17,21 +12,10 @@ if (empty($_GET['id']) || !ctype_digit($_GET['id'])) {
 $id = $_GET['id'];
 
 /**
- * 2. Connexion à la base de données avec PDO
- * Attention, on précise ici deux options :
- * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
- * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
- * 
- * PS : Vous remarquez que ce sont les mêmes lignes que pour l'index.php ?!
- */
-$pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
-
-/**
  * 3. Vérification que l'article existe bel et bien
  */
+$pdo = getPDO();
+
 $query = $pdo->prepare('SELECT * FROM articles WHERE id = :id');
 $query->execute(['id' => $id]);
 if ($query->rowCount() === 0) {
@@ -47,5 +31,4 @@ $query->execute(['id' => $id]);
 /**
  * 5. Redirection vers la page d'accueil
  */
-header("Location: index.php");
-exit();
+redirect("index.php");
--- save-comment.php
+++ save-comment.php
@@ -1,18 +1,6 @@
 <?php
-
-/**
- * CE FICHIER DOIT ENREGISTRER UN NOUVEAU COMMENTAIRE EST REDIRIGER SUR L'ARTICLE !
- * 
- * On doit d'abord vérifier que toutes les informations ont été entrées dans le formulaire
- * Si ce n'est pas le cas : un message d'erreur
- * Sinon, on va sauver les informations
- * 
- * Pour sauvegarder les informations, ce serait bien qu'on soit sur que l'article qu'on essaye de commenter existe
- * Il faudra donc faire une première requête pour s'assurer que l'article existe
- * Ensuite on pourra intégrer le commentaire
- * 
- * Et enfin on pourra rediriger l'utilisateur vers l'article en question
- */
+require_once 'librairies/database.php';
+require_once 'librairies/utils.php';
 
 /**
  * 1. On vérifie que les données ont bien été envoyées en POST
@@ -44,21 +32,7 @@ if (!$author || !$article_id || !$content) {
     die("Votre formulaire a été mal rempli !");
 }
 
-/**
- * 2. Vérification que l'id de l'article pointe bien vers un article qui existe
- * Ca nécessite une connexion à la base de données puis une requête qui va aller chercher l'article en question
- * Si rien ne revient, la personne se fout de nous.
- * 
- * Attention, on précise ici deux options :
- * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
- * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
- * 
- * PS : Ca fait pas genre 3 fois qu'on écrit ces lignes pour se connecter ?! 
- */
-$pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
+$pdo = getPDO();
 
 $query = $pdo->prepare('SELECT * FROM articles WHERE id = :article_id');
 $query->execute(['article_id' => $article_id]);
@@ -73,5 +47,4 @@ $query = $pdo->prepare('INSERT INTO comments SET author = :author, content = :co
 $query->execute(compact('author', 'content', 'article_id'));
 
 // 4. Redirection vers l'article en question :
-header('Location: article.php?id=' . $article_id);
-exit();
+redirect('article.php?id=' . $article_id);
