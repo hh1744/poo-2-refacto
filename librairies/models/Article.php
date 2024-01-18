d1bad8f04956a4e6efb671f5816e5e14b5030d58
henintsoa.andrianarisoa <henintsoa.andrianarisoa@businessdecision.com>
2024-01-18 22:04:27 +0400

    Cours Heritage

--- librairies/models/Article.php
+++ librairies/models/Article.php
@@ -3,22 +3,5 @@ require_once 'librairies/models/Model.php';
 
 class Article extends Model
 {
-    public function findAll(): array
-    {
-        $resultats = $this->pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
-        return $resultats->fetchAll();
-    }
-
-    public function find(int $id)
-    {
-        $query = $this->pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
-        $query->execute(['article_id' => $id]);
-        return $query->fetch();
-    }
-
-    public function delete(int $id): void
-    {
-        $query = $this->pdo->prepare('DELETE FROM articles WHERE id = :id');
-        $query->execute(['id' => $id]);
-    }
+    protected string $table = 'articles';
 }
