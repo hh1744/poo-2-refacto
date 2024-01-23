6709c57351286321525d102aa6d82d7e1cac0a92
henintsoa.andrianarisoa <henintsoa.andrianarisoa@businessdecision.com>
2024-01-23 21:38:31 +0400

    Cours singeltion

--- librairies/Database.php
+++ librairies/Database.php
@@ -2,13 +2,17 @@
 
 class Database{
 
+    private static $instance = null;
+
     public static function getPDO(): PDO
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'gedimat', 'gedimat', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+        if(self::$instance === null) {
+            self::$instance = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'gedimat', 'gedimat', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        }
 
-        return $pdo;
+        return self::$instance;
     }
 }
